e1136083f3361bad34361bf8bf456e69c48079a4
Ben <benjisgarza@gmail.com>
2020-01-24 08:48:28 -0700

    Created generateEducatedCoords in computer module

--- scr/computer.js
+++ scr/computer.js
@@ -5,7 +5,30 @@ const computer = (() => {
     randomCoords.y = Math.floor(Math.random() * 10);
   return randomCoords;
 }
-  
-  return { generateRandomCoords}
+  function generateEducatedCoords(lastshot) {
+    let educatedCoords = {x:-5, y:-5};
+    while (educatedCoords.x < 0 || educatedCoords.x > 9 || educatedCoords.y < 0 || educatedCoords.y > 9) {
+      switch(Math.floor(Math.random() * 4)){
+        case 0:
+          educatedCoords.x = lastshot.x + 1
+          educatedCoords.y = lastshot.y
+          break;
+        case 1:
+          educatedCoords.x = lastshot.x - 1
+          educatedCoords.y = lastshot.y
+          break;
+        case 2:
+          educatedCoords.x = lastshot.x
+          educatedCoords.y = lastshot.y + 1
+          break;
+        case 3:
+          educatedCoords.x = lastshot.x
+          educatedCoords.y = lastshot.y - 1
+          break;
+      };
+    }
+    return educatedCoords;
+  }
+  return { generateRandomCoords, generateEducatedCoords }
 })()
 module.exports = computer;
