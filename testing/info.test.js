7bc529995e7490d1886630ed413848579a306c6d
Ben <benjisgarza@gmail.com>
2020-01-24 10:07:27 -0700

    Add test for info and add test for ship

---
+++ testing/info.test.js
@@ -0,0 +1,28 @@
+const info =  require('../src/info');
+
+let gameOverDiv = document.createElement('div');
+gameOverDiv.id = 'game-over';
+document.body.appendChild(gameOverDiv);
+
+let boardDiv = document.createElement('div');
+boardDiv.classList = 'board';
+document.body.appendChild(boardDiv);
+
+let territory = {};
+territory.sunkenShips = 5;
+
+test ('Displays correct message when ship is sunk', () => {
+  info.messages("player");
+  expect(gameOverDiv.innerHTML).toBe('One of the computers ships has been sunk');
+  
+  info.messages("NPC");
+  expect(gameOverDiv.innerHTML).toBe('One of your ships has been sunk');
+});
+
+test ('Displays correct message after win condition', () => {
+  info.checkForWin(territory, "player");
+  expect(gameOverDiv.innerHTML).toBe('You Win!');
+  
+  info.checkForWin(territory, "NPC");
+  expect(gameOverDiv.innerHTML).toBe('You Lose!');
+});
